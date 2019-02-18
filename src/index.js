6a21c25df23a841fef55beef1f9d81d7f826fb39
uniorunr <gagael23@live.com>
2019-02-18 23:27:30 +0300

    refactor: use ES6, add small changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    var numberOfCoins = {};
+    const numberOfCoins = {};
+
     if (currency <= 0) return {};
     if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-    var coins = ['H', 'Q', 'D', 'N', 'P'];
-    var valuesOfCoins = [50, 25, 10, 5, 1];
-    var currentCurrency = currency;
-    for (var i = 0; i < valuesOfCoins.length; i++) {
-        var quotient = Math.floor(currentCurrency / valuesOfCoins[i]);
-        var remainder = currentCurrency % valuesOfCoins[i];
+
+    const coins = ['H', 'Q', 'D', 'N', 'P'];
+    const valuesOfCoins = [50, 25, 10, 5, 1];
+    let currentCurrency = currency;
+    
+    valuesOfCoins.forEach((item, index) => {
+        const quotient = Math.floor(currentCurrency / item);
+        const remainder = currentCurrency % item;
         if (quotient) {
-            numberOfCoins[coins[i]] = quotient;
+            numberOfCoins[coins[index]] = quotient;
             currentCurrency = remainder;
         }
-    }
+    })
+
     return numberOfCoins;
 }
